0a9708c9110ba3035f9c1861e4169959701fc7f0
adityaabdulloh <adityaabdulloh.22004@mhs.unesa.ac.id>
2025-11-09 16:32:42 +0700

    composer update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import './bootstrap';
-import '../../appwrite-init';
 import 'bootstrap/scss/bootstrap.scss';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import * as bootstrap from 'bootstrap';
